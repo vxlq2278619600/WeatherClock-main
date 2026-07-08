545da72cf0c13c551d43ed91c6324e2c0b2dcb74
vxlq2278619600 <2278619600@qq.com>
2026-07-08 19:51:11 +0800

    docs: add verified ST7789 wiring and display debug notes

--- app/main.c
+++ app/main.c
@@ -2,10 +2,78 @@
 #include "FreeRTOS.h"
 #include "task.h"
 #include "workqueue.h"
+#include "app.h"
 #include "ui.h"
+#include "wifi.h"
+#include "page.h"
+
+#define ENABLE_LCD_SELF_TEST 1
+#define LCD_TEST_PAGE_DELAY_MS 1000
 
 extern void board_lowlevel_init(void);
 extern void board_init(void);
+
+static void lcd_test_fill_page(uint16_t bg_color, uint16_t fg_color, const char *label)
+{
+    ui_fill_color(0, 0, UI_WIDTH - 1, UI_HEIGHT - 1, bg_color);
+    ui_write_string(70, 140, label, fg_color, bg_color, &font32_maple_bold);
+    vTaskDelay(pdMS_TO_TICKS(LCD_TEST_PAGE_DELAY_MS));
+}
+
+static void lcd_test_draw_graphics_page(void)
+{
+    const uint16_t bg_color = 0x0000;
+
+    ui_fill_color(0, 0, UI_WIDTH - 1, UI_HEIGHT - 1, bg_color);
+
+    /* Rectangle border */
+    ui_fill_color(10, 10, 229, 13, 0xFFFF);
+    ui_fill_color(10, 306, 229, 309, 0xFFFF);
+    ui_fill_color(10, 10, 13, 309, 0xFFFF);
+    ui_fill_color(226, 10, 229, 309, 0xFFFF);
+
+    /* Horizontal and vertical guide lines */
+    ui_fill_color(20, 70, 219, 73, 0xF800);
+    ui_fill_color(20, 140, 219, 143, 0x07E0);
+    ui_fill_color(20, 210, 219, 213, 0x001F);
+    ui_fill_color(118, 20, 121, 299, mkcolor(255, 255, 0));
+
+    ui_write_string(24, 26, "TOP LEFT", 0xFFFF, bg_color, &font20_maple_bold);
+    ui_write_string(72, 150, "CENTER", 0xFFFF, bg_color, &font24_maple_bold);
+    ui_write_string(32, 274, "Weather Clock", 0xFFFF, bg_color, &font20_maple_bold);
+
+    vTaskDelay(pdMS_TO_TICKS(LCD_TEST_PAGE_DELAY_MS));
+}
+
+static void lcd_test_draw_text_page(void)
+{
+    const uint16_t bg_color = 0x0000;
+    const uint16_t fg_color = 0xFFFF;
+
+    ui_fill_color(0, 0, UI_WIDTH - 1, UI_HEIGHT - 1, bg_color);
+    ui_write_string(20, 30, "ST7789 OK", fg_color, bg_color, &font32_maple_bold);
+    ui_write_string(20, 90, "SPI LCD TEST", mkcolor(0, 255, 234), bg_color, &font24_maple_bold);
+    ui_write_string(20, 145, "Hello STM32", fg_color, bg_color, &font24_maple_bold);
+    ui_write_string(20, 195, "1234567890", mkcolor(255, 255, 0), bg_color, &font24_maple_bold);
+    ui_write_string(20, 245, "Weather Clock", fg_color, bg_color, &font24_maple_bold);
+    vTaskDelay(pdMS_TO_TICKS(LCD_TEST_PAGE_DELAY_MS));
+}
+
+static void ST7789_DisplaySelfTest(void)
+{
+    printf("[LCD TEST] ST7789 display self test start\r\n");
+
+    lcd_test_fill_page(0x0000, 0xFFFF, "BLACK");
+    lcd_test_fill_page(0xFFFF, 0x0000, "WHITE");
+    lcd_test_fill_page(0xF800, 0xFFFF, "RED");
+    lcd_test_fill_page(0x07E0, 0x0000, "GREEN");
+    lcd_test_fill_page(0x001F, 0xFFFF, "BLUE");
+
+    lcd_test_draw_text_page();
+    lcd_test_draw_graphics_page();
+
+    printf("[LCD TEST] ST7789 display self test done\r\n");
+}
 
 static void main_init(void *param)
 {
@@ -14,19 +82,18 @@ static void main_init(void *param)
     board_init();
     ui_init();
 
-    printf("[LCD_TEST] start\r\n");
+#if ENABLE_LCD_SELF_TEST
+    ST7789_DisplaySelfTest();
+#endif
 
-    ui_fill_color(0, 0, UI_WIDTH - 1, UI_HEIGHT - 1, mkcolor(0, 0, 0));
-    ui_fill_color(20, 20, 69, 89, mkcolor(255, 0, 0));
-    ui_fill_color(75, 20, 124, 89, mkcolor(0, 255, 0));
-    ui_fill_color(130, 20, 179, 89, mkcolor(0, 0, 255));
-    ui_fill_color(185, 20, 234, 89, mkcolor(255, 255, 255));
+    welcome_page_display();
 
-    ui_write_string(20, 120, "ST7789 OK", mkcolor(255, 255, 255), mkcolor(0, 0, 0), &font32_maple_bold);
-    ui_write_string(20, 170, "SPI LCD TEST", mkcolor(0, 255, 234), mkcolor(0, 0, 0), &font24_maple_bold);
-    ui_write_string(20, 220, "If you see this, LCD works", mkcolor(255, 255, 0), mkcolor(0, 0, 0), &font16_maple);
+    wifi_init();
+    wifi_page_display();
+    wifi_wait_connect();
 
-    printf("[LCD_TEST] pattern sent\r\n");
+    main_page_display();
+    app_init();
 
     while (1)
     {
